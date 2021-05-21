b94a8df0e4592e3672cbb1eb91ba797a3b023bce
Peritract <peritract@hotmail.co.uk>
2021-05-21 14:47:55 +0100

    incomplete version

--- app.py
+++ app.py
@@ -1,41 +1,11 @@
 # Imports
 
-import json
 
-import web
-
-from scripts.predict import predict_penguin
 
 # Set up the URL routes
 
-urls = (
-    '/', 'Index',
-    '/predict', 'Predict'
-)
+
 
 # Define how the app will respond to routes
 
-class Index:
-    def GET(self):
-        return "This app provides penguin-prediction services."
 
-
-class Predict:
-    def POST(self):
-        # Extract the JSON data from the request
-        data = json.loads(web.data())
-
-        # Make the prediction
-        pred = predict_penguin(data)
-
-        # Return the response
-        return json.dumps({'prediction': pred})
-
-
-if __name__ == "__main__":
-
-    # Create an application with the routes set up
-    app = web.application(urls, globals())
-
-    # Run the app
-    app.run()
